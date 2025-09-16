da5b0c9712c970167083b6bfc2dffdcd65809a1b
Moenul <moenul22@gmail.com>
2025-09-16 23:34:28 +0600

    fix axios base url

--- assets/wp-vue/src/composable/useAxios.ts
+++ assets/wp-vue/src/composable/useAxios.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { has } from "lodash";
 
 const _getBaseURL = () => {
-  // return import.meta.env.VITE_APP_API_URL || "";
-  return "http://crafely.local/wp-json/ai-smart-sales/v1";
+  return import.meta.env.VITE_APP_API_URL || "";
 };
 
 const _getHeaders = () => {
